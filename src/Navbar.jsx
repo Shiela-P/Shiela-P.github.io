c87bf51b0073cf370f6ba2d231043c6e393ef721
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-29 20:59:26 -0400

    Creating and styling Navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (  
+        <nav className='navbar'>
+           <img src="public/favicon.ico"></img>
+            <a>Projects</a>
+            <a>About</a>
+            <a>Connect</a>
+            <a>Resume</a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
