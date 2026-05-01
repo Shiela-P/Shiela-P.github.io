60a7a6eee317f7928f12ed5269feab6897f6a435
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-30 23:06:39 -0400

    "finished" my navbar (for now). Also finished (maybe?) my hero image and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './Hero'
 import Navbar from './Navbar'
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
     
   )
---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return ( 
+        <div className="hero">
+            <img src="src/assets/hero.jpg"/>
+            <div className="title">
+                <h1>Shiela Peters</h1>
+                <h1 className="portfolio">Portfolio</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default Hero;
