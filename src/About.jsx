ca0b03d6ff13e6ce4348f3ed7bb6cf740dea106a
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-05-02 17:59:16 -0400

    Added an image to the baout section to add a bit more personality and story

--- src/About.jsx
+++ src/About.jsx
@@ -2,9 +2,14 @@ const About = () => {
     return ( 
         <div className="about">
             <h2>Hello there!</h2>
-            <p>I’m a creative frontend developer who loves turning ideas into delightful, usable products. Right now, I primarily make web-based projects, but I’m always open to learning new technologies and skills.</p>
-            <p>I’m also a college junior working toward a B.S. in Internet & Web Development with a minor in Human Systems Engineering, which helps me blend front-end know-how with user-centered thinking. This site is a showcase of my skills and projects and is being updated as I learn more.</p>
-            <p>I’m open to career opportunities and would love to connect. Feel free to reach out!</p>
+            <div className="about-flex">
+                <img src="src/assets/about.png" />
+                    <div>
+                        <p>I’m a creative frontend developer who loves turning ideas into delightful, usable products. Right now, I primarily make web-based projects, but I’m always open to learning new technologies and skills.</p>
+                        <p>I’m also a college junior working toward a B.S. in Internet & Web Development with a minor in Human Systems Engineering, which helps me blend front-end know-how with user-centered thinking. This site is a showcase of my skills and projects and is being updated as I learn more.</p>
+                        <p>I’m open to career opportunities and would love to connect. Feel free to reach out!</p>
+                    </div>
+            </div>
         </div>
      );
 }
