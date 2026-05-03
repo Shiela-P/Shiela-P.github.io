f436f612ba29d9ec91ba83051938293155b6536f
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-05-02 22:58:12 -0400

    Completed card layout and styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,13 @@ const Card = () => {
                 <h1>Title</h1>
             </div>
             <h2>Project Type</h2>
-                
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. </p>   
+            <div className="skills">
+                <p>skill</p>
+                <p>skill</p>
+                <p>skill</p>
+                <p>skill</p>
+            </div>
         </div>
      );
 }
